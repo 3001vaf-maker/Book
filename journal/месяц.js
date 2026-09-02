4de4444ec49b3bf13f6e9f62b7215db0d468f7bb
3001vaf-maker <30.01vaf@gmail.com>
2026-09-02 11:47:22 +0300

    Add Journal Month view entry point

---
+++ journal/месяц.js
@@ -0,0 +1,5 @@
+import { emptyState } from '../ui/ui.js';
+
+export function renderJournalMonth(root) {
+  root.innerHTML = emptyState('Месяц', 'Представление Месяца подготовлено для последующей реализации.');
+}
