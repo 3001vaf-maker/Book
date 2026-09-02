c9e952be1f73648c7ce6f30c7426a1e49ba72d30
3001vaf-maker <30.01vaf@gmail.com>
2026-09-02 11:47:28 +0300

    Add Journal List view entry point

---
+++ journal/список.js
@@ -0,0 +1,5 @@
+import { emptyState } from '../ui/ui.js';
+
+export function renderJournalList(root) {
+  root.innerHTML = emptyState('Список', 'Представление Списка подготовлено для последующей реализации.');
+}
