dc9afaeed5fc08312a3c65e368f531f331f38461
3001vaf-maker <30.01vaf@gmail.com>
2026-09-02 11:47:18 +0300

    Add Journal Day view entry point

---
+++ journal/день.js
@@ -0,0 +1,5 @@
+import { emptyState } from '../ui/ui.js';
+
+export function renderJournalDay(root) {
+  root.innerHTML = emptyState('День', 'Представление Дня подготовлено для последующей реализации.');
+}
