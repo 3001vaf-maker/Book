210b491617db37e0c37cfd76ba0d0792cdeb8899
3001vaf-maker <30.01vaf@gmail.com>
2026-09-03 21:01:32 +0300

    Добавить проваливание из месяца в день

--- journal/месяц.js
+++ journal/месяц.js
@@ -1,7 +1,7 @@
 import { initCalendar } from '../ui/ui.js?v=journal-month-worktime-20260903';
 import { getWorkingDays, getWorkingDates } from '../core/workplace-time.js?v=journal-worktime-20260903';
 
-export function renderJournalMonth(root, { workplaceId = '' } = {}) {
+export function renderJournalMonth(root, { workplaceId = '', onDateSelect = () => {} } = {}) {
   const render = (month = new Date(new Date().getFullYear(), new Date().getMonth(), 1)) => {
     root.innerHTML = '<div data-journal-month-calendar></div>';
     const workingDays = getWorkingDays();
@@ -9,7 +9,11 @@ export function renderJournalMonth(root, { workplaceId = '' } = {}) {
       month,
       workingDates: getWorkingDates(workingDays, workplaceId, month),
       renderDateContent: () => '',
-      onDateSelect: () => {},
+      onDateSelect: (dateKey) => {
+        const [year, monthNumber, day] = String(dateKey).split('-').map(Number);
+        if (!year || !monthNumber || !day) return;
+        onDateSelect(new Date(year, monthNumber - 1, day));
+      },
       onMonthChange: (nextMonth) => render(nextMonth),
     });
   };
