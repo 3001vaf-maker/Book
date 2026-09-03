c1c4c4d0fb23ed342b6ae6f4710c44ef628891bd
3001vaf-maker <30.01vaf@gmail.com>
2026-09-03 19:57:40 +0300

    Refresh shared Calendar module version

--- ui/calendar/index.js
+++ ui/calendar/index.js
@@ -1,2 +1,2 @@
-export { calendar, initCalendar, dateNavigator, initDateNavigator } from './calendar.js?v=graph-20260902';
+export { calendar, initCalendar, dateNavigator, initDateNavigator } from './calendar.js?v=journal-day-20260903';
 export { initMultiSelect } from '../selection/multi-select.js?v=graph-20260902';
