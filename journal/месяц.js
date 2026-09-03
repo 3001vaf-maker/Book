fc490d967ebcab918b8e5dc1e55a0fda5470c122
3001vaf-maker <30.01vaf@gmail.com>
2026-09-03 20:38:32 +0300

    feat: connect journal month to workplace worktime

--- journal/месяц.js
+++ journal/месяц.js
@@ -1,10 +1,18 @@
-import { initCalendar } from '../ui/ui.js';
+import { initCalendar } from '../ui/ui.js?v=journal-month-worktime-20260903';
+import { getWorkingDays, getWorkingDates } from '../core/workplace-time.js?v=journal-worktime-20260903';
 
-export function renderJournalMonth(root) {
-  root.innerHTML = '<div data-journal-month-calendar></div>';
-  initCalendar(root.querySelector('[data-journal-month-calendar]'), {
-    workingDates: [],
-    renderDateContent: () => '',
-    onDateSelect: () => {},
-  });
+export function renderJournalMonth(root, { workplaceId = '' } = {}) {
+  const render = (month = new Date(new Date().getFullYear(), new Date().getMonth(), 1)) => {
+    root.innerHTML = '<div data-journal-month-calendar></div>';
+    const workingDays = getWorkingDays();
+    initCalendar(root.querySelector('[data-journal-month-calendar]'), {
+      month,
+      workingDates: getWorkingDates(workingDays, workplaceId, month),
+      renderDateContent: () => '',
+      onDateSelect: () => {},
+      onMonthChange: (nextMonth) => render(nextMonth),
+    });
+  };
+
+  render();
 }
