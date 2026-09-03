7b3dbe990589383d27ae3dd2f86fff2e0b05a9eb
3001vaf-maker <30.01vaf@gmail.com>
2026-09-03 19:57:09 +0300

    Expose shared date navigator through Calendar UI

--- ui/calendar/index.js
+++ ui/calendar/index.js
@@ -1,2 +1,2 @@
-export { calendar, initCalendar } from './calendar.js?v=graph-20260902';
+export { calendar, initCalendar, dateNavigator, initDateNavigator } from './calendar.js?v=graph-20260902';
 export { initMultiSelect } from '../selection/multi-select.js?v=graph-20260902';
