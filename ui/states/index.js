a3723bed683136275917ce5c72de6e745ca1b0ca
3001vaf-maker <30.01vaf@gmail.com>
2026-09-05 13:10:04 +0300

    Export StateView from states UI

--- ui/states/index.js
+++ ui/states/index.js
@@ -1,4 +1,7 @@
 import { escapeHtml } from '../utils/escape-html.js';
+import { stateView, initStateView } from './state-view.js';
+
+export { stateView, initStateView };
 
 export function emptyState(title, text = '') {
   return `<div class="empty-state"><strong>${escapeHtml(title)}</strong>${text ? `<span>${escapeHtml(text)}</span>` : ''}</div>`;
