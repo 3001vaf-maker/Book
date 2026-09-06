df2037e98d6b115746ecb7e9b7a58336e86dc087
3001vaf-maker <30.01vaf@gmail.com>
2026-09-06 23:33:08 +0300

    feat: export shared month-day picker

--- ui/calendar/index.js
+++ ui/calendar/index.js
@@ -1,2 +1,2 @@
-export { calendar, initCalendar, dateNavigator, initDateNavigator } from './calendar.js?v=journal-day-20260903';
+export { calendar, initCalendar, monthDayPicker, initMonthDayPickers, dateNavigator, initDateNavigator } from './calendar.js?v=month-day-20260906';
 export { initMultiSelect } from '../selection/multi-select.js?v=graph-20260902';
