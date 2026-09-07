3986bf34f51addf0ad52428feb0178040fb3a20b
3001vaf-maker <30.01vaf@gmail.com>
2026-09-08 02:00:32 +0300

    refresh graph partial conflict bundle

--- core.js
+++ core.js
@@ -1,6 +1,6 @@
 import { renderMain } from './main/main.js';
 import { renderJournal } from './journal/journal.js?v=graph-architecture-20260908';
-import { renderTimetable } from './timetable/timetable.js?v=graph-multi-conflict-20260908';
+import { renderTimetable } from './timetable/timetable.js?v=graph-partial-conflict-20260908';
 import { renderChat } from './chat/chat.js';
 import { renderSettings } from './settings/settings.js?v=tag-manager-ui-20260908';
 import { getWorkplaces as getWorkplaceEntities } from './settings/profile/workplaces/data.js';
