aff6ebef55a73de133d353a5bd89d6eae9b3aafb
3001vaf-maker <30.01vaf@gmail.com>
2026-09-09 00:14:53 +0300

    refactor: canonicalize calendar exports

--- ui/calendar/index.js
+++ ui/calendar/index.js
@@ -1,2 +1,2 @@
-export { calendar, initCalendar, monthDayPicker, initMonthDayPickers, dateNavigator, initDateNavigator } from './calendar.js?v=schedule-indicators-20260908';
-export { initMultiSelect } from '../selection/multi-select.js?v=graph-20260902';
+export { calendar, initCalendar, monthDayPicker, initMonthDayPickers, dateNavigator, initDateNavigator } from './calendar.js';
+export { initMultiSelect } from '../selection/multi-select.js';
