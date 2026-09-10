563e5f2888bacf0c2f1c7dcdd88c35c8d2e956bd
3001vaf-maker <30.01vaf@gmail.com>
2026-09-10 22:30:23 +0300

    Register profile module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { HealthController } from './health.controller';
 import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { WorkspaceModule } from './workspace/workspace.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [AuthModule, WorkspaceModule],
+  imports: [AuthModule, WorkspaceModule, ProfileModule],
   controllers: [HealthController],
   providers: [PrismaService],
 })
