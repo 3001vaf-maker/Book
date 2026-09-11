10e9e778d2a260407e2609c6ebace64c66061853
3001vaf-maker <30.01vaf@gmail.com>
2026-09-11 16:24:08 +0300

    Add single Day Core facade

---
+++ core/day/index.js
@@ -0,0 +1,19 @@
+// Public WorkPlan Day Core contract.
+// Manifestations import this facade; storage stays internal to the Day owner.
+export { getDay, getDayTime, getDays, getDaysForDate } from './read.js';
+export {
+  dayDate,
+  getDayDraftScheduleConflicts,
+  getScheduleConflictsForDays,
+  hasScheduleConflictForDays,
+  findSuggestedDayInterval,
+  resolveDayTime,
+  totalDayMinutes,
+} from './rules.js';
+export {
+  createDay,
+  getDayRemovalConflicts,
+  removeDay,
+  saveDays,
+  updateDayTime,
+} from './service.js';
