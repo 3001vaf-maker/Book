9d25f7e462bbf6904378b9434762b27e314f7bb8
3001vaf-maker <30.01vaf@gmail.com>
2026-09-11 17:02:05 +0300

    Expose Record public contract

---
+++ core/record/index.js
@@ -0,0 +1,34 @@
+// Public Record Core contract.
+export {
+  getActiveRecordCountForDay,
+  getRecord,
+  getRecords,
+  getRecordsForDay,
+  readRecord,
+} from './read.js';
+export {
+  cancelRecord,
+  checkRecordTime,
+  createRecord,
+  deleteRecord,
+  moveRecord,
+  removeRecord,
+  setRecordAttendance,
+  setRecordConfirmed,
+  updateRecord,
+} from './service.js';
+export {
+  appendRecordEvent,
+  ensureLegacyRecordEvents,
+  getAllRecordEvents,
+  getRecordEvents,
+  hasRecordEvent,
+  RECORD_EVENT_TYPES,
+} from './events.js';
+export {
+  isRecordCompletedSide,
+  projectRecordLifecycle,
+  recordActivityTime,
+  recordAppointmentTime,
+  recordVisualState,
+} from './state.js';
