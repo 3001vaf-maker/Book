cd89c855b76d3af1b9e73303f8531340ff50f421
3001vaf-maker <30.01vaf@gmail.com>
2026-09-11 22:30:16 +0300

    Format person phone for presentation

--- main/clients/presentation.js
+++ main/clients/presentation.js
@@ -1,9 +1,10 @@
+import { formatPhone } from '../../core/phone/index.js';
 import { getUEI } from '../../core/uei.js';
 
 export function clientDisplay(person = {}) {
   return {
     uei: person.uei || getUEI('person', person.key) || '',
     name: [person?.name, person?.surname].filter(Boolean).join(' ') || 'Без имени',
-    phone: person?.phones?.[0] || person?.phone || '',
+    phone: formatPhone(person?.phones?.[0] || person?.phone || ''),
   };
 }
