5c95eecb5410b840517e6b4dadab00f181cfb93a
3001vaf-maker <30.01vaf@gmail.com>
2026-09-13 02:47:29 +0300

    Register canonical booking consent controller

--- server/src/online-booking/online-booking.module.ts
+++ server/src/online-booking/online-booking.module.ts
@@ -5,13 +5,14 @@ import { DocumentStateModule } from '../document-state/document-state.module';
 import { ProfileModule } from '../profile/profile.module';
 import { PrismaService } from '../prisma.service';
 import { BookingAccountGuard } from './booking-account.guard';
+import { BookingConsentController } from './booking-consent.controller';
 import { BookingRequiredConsentGuard } from './booking-required-consent.guard';
 import { OnlineBookingController } from './online-booking.controller';
 import { OnlineBookingService } from './online-booking.service';
 
 @Module({
   imports: [AuthModule, BusinessStateModule, DocumentStateModule, ProfileModule],
-  controllers: [OnlineBookingController],
+  controllers: [OnlineBookingController, BookingConsentController],
   providers: [OnlineBookingService, BookingAccountGuard, BookingRequiredConsentGuard, PrismaService],
 })
 export class OnlineBookingModule {}
