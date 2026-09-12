679a8fe85e46b3f98fb0996f4ad4bf657f17ecf8
3001vaf-maker <30.01vaf@gmail.com>
2026-09-13 02:44:54 +0300

    Register booking consent guard

--- server/src/online-booking/online-booking.module.ts
+++ server/src/online-booking/online-booking.module.ts
@@ -5,12 +5,13 @@ import { DocumentStateModule } from '../document-state/document-state.module';
 import { ProfileModule } from '../profile/profile.module';
 import { PrismaService } from '../prisma.service';
 import { BookingAccountGuard } from './booking-account.guard';
+import { BookingRequiredConsentGuard } from './booking-required-consent.guard';
 import { OnlineBookingController } from './online-booking.controller';
 import { OnlineBookingService } from './online-booking.service';
 
 @Module({
   imports: [AuthModule, BusinessStateModule, DocumentStateModule, ProfileModule],
   controllers: [OnlineBookingController],
-  providers: [OnlineBookingService, BookingAccountGuard, PrismaService],
+  providers: [OnlineBookingService, BookingAccountGuard, BookingRequiredConsentGuard, PrismaService],
 })
 export class OnlineBookingModule {}
