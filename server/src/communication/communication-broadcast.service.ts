7fec8067394a471f6fb7764928ff46ca68b286eb
3001vaf-maker <30.01vaf@gmail.com>
2026-09-13 09:17:19 +0300

    Use consent policy extension for broadcast eligibility

--- server/src/communication/communication-broadcast.service.ts
+++ server/src/communication/communication-broadcast.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { randomUUID } from 'node:crypto';
 import { BusinessStateService } from '../business-state/business-state.service';
-import { DocumentStateService } from '../document-state/document-state.service';
+import { ConsentPolicyService } from '../document-state/consent-policy.service';
 import { PrismaService } from '../prisma.service';
 import { CommunicationService } from './communication.service';
 import { CommunicationDispatchService } from './communication-dispatch.service';
@@ -25,7 +25,7 @@ export class CommunicationBroadcastService {
   constructor(
     private readonly prisma: PrismaService,
     private readonly businessState: BusinessStateService,
-    private readonly documents: DocumentStateService,
+    private readonly documents: ConsentPolicyService,
     private readonly communications: CommunicationService,
     private readonly dispatch: CommunicationDispatchService,
   ) {}
