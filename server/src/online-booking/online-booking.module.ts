30e552c464a817e00db318a6c20589d3b6bb7343
3001vaf-maker <30.01vaf@gmail.com>
2026-09-13 09:21:17 +0300

    Add client-card link service without changing booking modules

--- server/src/online-booking/online-booking.module.ts
+++ server/src/online-booking/online-booking.module.ts
@@ -9,12 +9,13 @@ import { PrismaService } from '../prisma.service';
 import { BookingAccountGuard } from './booking-account.guard';
 import { BookingConsentController } from './booking-consent.controller';
 import { BookingRequiredConsentGuard } from './booking-required-consent.guard';
+import { ClientCardLinkService } from './client-card-link.service';
 import { OnlineBookingController } from './online-booking.controller';
 import { OnlineBookingService } from './online-booking.service';
 
 @Module({
   imports: [AuthModule, BusinessStateModule, CommunicationModule, DocumentStateModule, NotificationModule, ProfileModule],
   controllers: [OnlineBookingController, BookingConsentController],
-  providers: [OnlineBookingService, BookingAccountGuard, BookingRequiredConsentGuard, PrismaService],
+  providers: [OnlineBookingService, BookingAccountGuard, BookingRequiredConsentGuard, ClientCardLinkService, PrismaService],
 })
 export class OnlineBookingModule {}
