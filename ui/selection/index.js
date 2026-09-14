a5ee0dc605491a9086ea95e510425d19ca865dba
3001vaf-maker <30.01vaf@gmail.com>
2026-09-14 16:01:09 +0300

    Export shared segmented control

--- ui/selection/index.js
+++ ui/selection/index.js
@@ -1,2 +1,3 @@
 export { initMultiSelect } from './multi-select.js';
 export { checkList, collectCheckList, initCheckList } from './check-list.js';
+export { segmentControl, initSegmentControls } from './segment-control.js';
