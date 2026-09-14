545cf35216495c49da320e807e0c9659419108f1
3001vaf-maker <30.01vaf@gmail.com>
2026-09-14 21:09:13 +0300

    Add master invitation module

---
+++ server/src/master-invitation/master-invitation.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { PrismaService } from '../prisma.service';
+import { TransactionalEmailModule } from '../transactional-email/transactional-email.module';
+import { MasterInvitationController } from './master-invitation.controller';
+import { MasterInvitationService } from './master-invitation.service';
+
+@Module({
+  imports: [AuthModule, TransactionalEmailModule],
+  controllers: [MasterInvitationController],
+  providers: [MasterInvitationService, PrismaService],
+  exports: [MasterInvitationService],
+})
+export class MasterInvitationModule {}
