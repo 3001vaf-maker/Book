5526c9cf388e1efe92401f8acd3dd2daf617707e
3001vaf-maker <30.01vaf@gmail.com>
2026-09-16 09:30:35 +0300

    Start completion runtime through record public contract

--- core/bootstrap.js
+++ core/bootstrap.js
@@ -4,6 +4,7 @@ import { prepareTelegramEntryAuth } from './telegram-entry-auth.js';
 const telegramAuth = await prepareTelegramEntryAuth();
 if (!telegramAuth?.halt) {
   await import('../core.js');
-  await import('./record/completion-runtime.js');
+  const { startRecordCompletionRuntime } = await import('./record/index.js');
+  startRecordCompletionRuntime();
   await import('./incoming-notifications/runtime.js');
 }
