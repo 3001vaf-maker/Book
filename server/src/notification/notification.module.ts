e65183e74e01a21bd4b110212860d285e770d401
3001vaf-maker <30.01vaf@gmail.com>
2026-09-16 09:08:38 +0300

    Wire notification templates and lifecycle bridge

--- server/src/notification/notification.module.ts
+++ server/src/notification/notification.module.ts
@@ -1,16 +1,18 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { BusinessStateModule } from '../business-state/business-state.module';
 import { DocumentStateModule } from '../document-state/document-state.module';
 import { PrismaService } from '../prisma.service';
+import { BookingLifecycleNotificationService } from './booking-lifecycle-notification.service';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
+import { NotificationTemplateService } from './notification-template.service';
 import { WebPushService } from './web-push.service';
 
 @Module({
-  imports: [AuthModule, BusinessStateModule, DocumentStateModule],
+  imports: [AuthModule, forwardRef(() => BusinessStateModule), DocumentStateModule],
   controllers: [NotificationController],
-  providers: [NotificationService, WebPushService, PrismaService],
-  exports: [NotificationService, WebPushService],
+  providers: [NotificationService, NotificationTemplateService, BookingLifecycleNotificationService, WebPushService, PrismaService],
+  exports: [NotificationService, NotificationTemplateService, BookingLifecycleNotificationService, WebPushService],
 })
 export class NotificationModule {}
