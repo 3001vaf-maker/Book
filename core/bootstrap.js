89befb558a585079dac692d7bae7bde7a8f7a37b
3001vaf-maker <30.01vaf@gmail.com>
2026-09-16 09:26:17 +0300

    Start record completion runtime

--- core/bootstrap.js
+++ core/bootstrap.js
@@ -4,5 +4,6 @@ import { prepareTelegramEntryAuth } from './telegram-entry-auth.js';
 const telegramAuth = await prepareTelegramEntryAuth();
 if (!telegramAuth?.halt) {
   await import('../core.js');
+  await import('./record/completion-runtime.js');
   await import('./incoming-notifications/runtime.js');
 }
