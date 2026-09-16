b0cc830a83d844465798d075727f5a0d27c8dd59
3001vaf-maker <30.01vaf@gmail.com>
2026-09-16 09:32:20 +0300

    Preserve Telegram bootstrap contract with completion runtime

--- core/bootstrap.js
+++ core/bootstrap.js
@@ -2,8 +2,8 @@ import './access-runtime.js';
 import { prepareTelegramEntryAuth } from './telegram-entry-auth.js';
 
 const telegramAuth = await prepareTelegramEntryAuth();
+if (!telegramAuth?.halt) await import('../core.js');
 if (!telegramAuth?.halt) {
-  await import('../core.js');
   const { startRecordCompletionRuntime } = await import('./record/index.js');
   startRecordCompletionRuntime();
   await import('./incoming-notifications/runtime.js');
