f294e10e7f8fb48f134c8a08889fd028398c031a
3001vaf-maker <30.01vaf@gmail.com>
2026-09-16 09:00:14 +0300

    Start incoming notification runtime

--- core/bootstrap.js
+++ core/bootstrap.js
@@ -2,4 +2,7 @@ import './access-runtime.js';
 import { prepareTelegramEntryAuth } from './telegram-entry-auth.js';
 
 const telegramAuth = await prepareTelegramEntryAuth();
-if (!telegramAuth?.halt) await import('../core.js');
+if (!telegramAuth?.halt) {
+  await import('../core.js');
+  await import('./incoming-notifications/runtime.js');
+}
