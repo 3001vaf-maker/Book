99798e461e645351e8ce786c5a232187baf41ca4
3001vaf-maker <30.01vaf@gmail.com>
2026-09-18 17:12:47 +0300

    Use neutral invitation dependency

--- server/src/manual-invitation/manual-invitation.module.ts
+++ server/src/manual-invitation/manual-invitation.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
-import { MasterInvitationModule } from '../master-invitation/master-invitation.module';
+import { UserInvitationModule } from '../user-invitation/user-invitation.module';
 import { PrismaService } from '../prisma.service';
 import { PlatformAdminGuard } from '../saas-admin/platform-admin.guard';
 import {
@@ -10,7 +10,7 @@ import {
 import { ManualInvitationService } from './manual-invitation.service';
 
 @Module({
-  imports: [AuthModule, MasterInvitationModule],
+  imports: [AuthModule, UserInvitationModule],
   controllers: [ManualInvitationAdminController, ManualInvitationController],
   providers: [ManualInvitationService, PlatformAdminGuard, PrismaService],
 })
