d2fc8df260929954b2c507c051f623f440a5b1fe
3001vaf-maker <30.01vaf@gmail.com>
2026-09-18 16:52:12 +0300

    Remove legal runtime from manual invitations

--- server/src/manual-invitation/manual-invitation.module.ts
+++ server/src/manual-invitation/manual-invitation.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
-import { LegalRuntimeModule } from '../legal-runtime/legal-runtime.module';
 import { MasterInvitationModule } from '../master-invitation/master-invitation.module';
 import { PrismaService } from '../prisma.service';
 import { PlatformAdminGuard } from '../saas-admin/platform-admin.guard';
@@ -11,7 +10,7 @@ import {
 import { ManualInvitationService } from './manual-invitation.service';
 
 @Module({
-  imports: [AuthModule, LegalRuntimeModule, MasterInvitationModule],
+  imports: [AuthModule, MasterInvitationModule],
   controllers: [ManualInvitationAdminController, ManualInvitationController],
   providers: [ManualInvitationService, PlatformAdminGuard, PrismaService],
 })
