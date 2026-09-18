541d5c1b0ece4562ffc05885fbac9a6e8a28e4cb
3001vaf-maker <30.01vaf@gmail.com>
2026-09-18 23:22:56 +0300

    Expose platform document repository to Admin

--- server/src/saas-admin/saas-admin.module.ts
+++ server/src/saas-admin/saas-admin.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { MasterInvitationModule } from '../master-invitation/master-invitation.module';
 import { PrismaService } from '../prisma.service';
+import { PlatformDocumentsModule } from '../platform-documents/platform-documents.module';
 import { SaasAccessModule } from '../saas-access/saas-access.module';
 import { PlatformAdminGuard } from './platform-admin.guard';
 import { SaasAdminController } from './saas-admin.controller';
 import { SaasAdminService } from './saas-admin.service';
 
 @Module({
-  imports: [AuthModule, SaasAccessModule, MasterInvitationModule],
+  imports: [AuthModule, SaasAccessModule, MasterInvitationModule, PlatformDocumentsModule],
   controllers: [SaasAdminController],
   providers: [SaasAdminService, PlatformAdminGuard, PrismaService],
 })
