35aa3368722c50c649d857c301aaf0749c294214
3001vaf-maker <30.01vaf@gmail.com>
2026-09-18 16:51:14 +0300

    Use core notifications without legal wrapper

--- server/src/notification/notification.module.ts
+++ server/src/notification/notification.module.ts
@@ -2,12 +2,10 @@ import { forwardRef, Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { BusinessStateModule } from '../business-state/business-state.module';
 import { DocumentStateModule } from '../document-state/document-state.module';
-import { LegalRuntimeModule } from '../legal-runtime/legal-runtime.module';
 import { PrismaService } from '../prisma.service';
 import { ProfileModule } from '../profile/profile.module';
 import { SaasAccessModule } from '../saas-access/saas-access.module';
 import { BookingLifecycleNotificationService } from './booking-lifecycle-notification.service';
-import { LegalNotificationService } from './legal-notification.service';
 import { NotificationController } from './notification.controller';
 import { NotificationReminderService } from './notification-reminder.service';
 import { NotificationService } from './notification.service';
@@ -15,10 +13,10 @@ import { NotificationTemplateService } from './notification-template.service';
 import { WebPushService } from './web-push.service';
 
 @Module({
-  imports: [AuthModule, forwardRef(() => BusinessStateModule), DocumentStateModule, LegalRuntimeModule, ProfileModule, SaasAccessModule],
+  imports: [AuthModule, forwardRef(() => BusinessStateModule), DocumentStateModule, ProfileModule, SaasAccessModule],
   controllers: [NotificationController],
   providers: [
-    { provide: NotificationService, useClass: LegalNotificationService },
+    NotificationService,
     NotificationTemplateService,
     BookingLifecycleNotificationService,
     NotificationReminderService,
