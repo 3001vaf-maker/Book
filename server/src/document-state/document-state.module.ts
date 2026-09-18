df2e15e40cdf8394bad0739749df2e7eb76e7ee7
3001vaf-maker <30.01vaf@gmail.com>
2026-09-18 23:32:28 +0300

    Connect server/src/document-state/document-state.module.ts to Admin templates

--- server/src/document-state/document-state.module.ts
+++ server/src/document-state/document-state.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaService } from '../prisma.service';
+import { PlatformDocumentsModule } from '../platform-documents/platform-documents.module';
 import { ConsentPolicyService } from './consent-policy.service';
 import { DocumentStateController } from './document-state.controller';
 import { DocumentStateService } from './document-state.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PlatformDocumentsModule],
   controllers: [DocumentStateController],
   providers: [PrismaService, DocumentStateService, ConsentPolicyService],
   exports: [DocumentStateService, ConsentPolicyService],
