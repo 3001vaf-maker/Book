03d388b5b34d571670891286a2f7552be401223c
3001vaf-maker <30.01vaf@gmail.com>
2026-09-18 16:59:51 +0300

    Remove legacy runtime module from business state

--- server/src/business-state/business-state.module.ts
+++ server/src/business-state/business-state.module.ts
@@ -1,6 +1,5 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
-import { LegalRuntimeModule } from '../legal-runtime/legal-runtime.module';
 import { NotificationModule } from '../notification/notification.module';
 import { PrismaService } from '../prisma.service';
 import { BusinessStateController } from './business-state.controller';
@@ -8,7 +7,7 @@ import { BusinessStateService } from './business-state.service';
 import { ClientContactRulesService } from './client-contact-rules.service';
 
 @Module({
-  imports: [AuthModule, LegalRuntimeModule, forwardRef(() => NotificationModule)],
+  imports: [AuthModule, forwardRef(() => NotificationModule)],
   controllers: [BusinessStateController],
   providers: [BusinessStateService, ClientContactRulesService, PrismaService],
   exports: [BusinessStateService, ClientContactRulesService],
