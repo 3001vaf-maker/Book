7ff469195f8aaa3cfb712711a70d20d2db8cd070
3001vaf-maker <30.01vaf@gmail.com>
2026-09-19 16:00:44 +0300

    Wire neutral invitation module into admin

--- server/src/saas-admin/saas-admin.module.ts
+++ server/src/saas-admin/saas-admin.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
-import { MasterInvitationModule } from '../master-invitation/master-invitation.module';
+import { InvitationModule } from '../invitation/invitation.module';
 import { DocumentRegistryModule } from '../document-registry/document-registry.module';
 import { PrismaService } from '../prisma.service';
 import { SaasAccessModule } from '../saas-access/saas-access.module';
@@ -9,7 +9,7 @@ import { SaasAdminController } from './saas-admin.controller';
 import { SaasAdminService } from './saas-admin.service';
 
 @Module({
-  imports: [AuthModule, SaasAccessModule, MasterInvitationModule, DocumentRegistryModule],
+  imports: [AuthModule, SaasAccessModule, InvitationModule, DocumentRegistryModule],
   controllers: [SaasAdminController],
   providers: [SaasAdminService, PlatformAdminGuard, PrismaService],
 })
