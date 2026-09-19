723818e9510978fcf18d964a4ba984fa92402520
3001vaf-maker <30.01vaf@gmail.com>
2026-09-19 16:30:13 +0300

    Add tenant invitation controller

---
+++ server/src/tenant-invitation/tenant-invitation.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { TenantInvitationService } from './tenant-invitation.service';
+
+@Controller('tenant-invitations')
+export class TenantInvitationController {
+  constructor(private readonly invitations: TenantInvitationService) {}
+
+  @Post('inspect')
+  inspect(@Body() body: { token?: unknown }) {
+    return this.invitations.inspect(body?.token);
+  }
+
+  @Post('accept')
+  accept(@Body() body: { token?: unknown; password?: unknown }) {
+    return this.invitations.accept(body || {});
+  }
+}
