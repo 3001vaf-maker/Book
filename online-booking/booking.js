9e8b68c6ad7f1ec82962aa1f2a0ead2657c36069
3001vaf-maker <30.01vaf@gmail.com>
2026-09-26 00:24:50 +0300

    Use direct Shared owners in booking

--- online-booking/booking.js
+++ online-booking/booking.js
@@ -24,29 +24,29 @@ import {
   submitAccountConsents,
 } from '../core/account/index.js';
 import { normalizeBookingSettings } from '../core/booking-settings/index.js';
+import { bookingChoiceCards, bookingThemeStyle, bookingTimeGroups } from '../ui/booking/index.js';
+import { button } from '../ui/buttons/index.js';
+import { initCalendar } from '../ui/calendar/index.js';
 import {
-  bookingChoiceCards,
-  bookingThemeStyle,
-  bookingTimeGroups,
-  button,
-  emptyState,
-  escapeHtml,
   field,
   formValidationMessage,
-  initCalendar,
   initPasswordFields,
-  initV2StickerSwipe,
-  initV2Swipe,
-  openNotice,
   passwordField,
   phoneField,
+} from '../ui/inputs/index.js';
+import { openNotice } from '../ui/modals/index.js';
+import { emptyState } from '../ui/states/index.js';
+import { escapeHtml } from '../ui/utils/escape-html.js';
+import {
+  initV2StickerSwipe,
+  initV2Swipe,
   v2Document,
   v2Header,
   v2LegalCards,
   v2ServiceStickers,
   v2Shell,
   v2Sticker,
-} from '../ui/ui.js';
+} from '../ui/v2/index.js';
 import { formError, formView } from '../ui/forms/index.js';
 import {
   bookingProcedureCost,
